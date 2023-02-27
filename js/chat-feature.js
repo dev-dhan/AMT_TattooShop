4ab91e3cab4b5347968116788f90a3fb7427576f
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-02-27 22:16:17 +1100

    updated chat feature

--- js/chat-feature.js
+++ js/chat-feature.js
@@ -1,6 +1,5 @@
-
 var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "100378746306263");
+chatbox.setAttribute("page_id", "116028498075589");
 chatbox.setAttribute("attribution", "biz_inbox");
 
 
